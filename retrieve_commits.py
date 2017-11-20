b7204ce0bde3d9801ba7d104bd58c08226a480c3
berg <cberg@tcd.ie>
2017-11-20 19:53:05 +0000

    tested on user defunkt -> seems to work

--- retrieve_commits.py
+++ retrieve_commits.py
@@ -11,78 +11,55 @@ class RepoCommitData(object):
         self.user_name = user_name
         self.auth_token = sys.argv[1]
         self.user_path = "data/%s"%user_name
+        self.commits_path = "retrieved_data/%s/"%self.user_name
+        if not os.path.exists(self.commits_path):
+            os.makedirs(self.commits_path)
 
     def walk_dirs(self):
-        for directory in os.walk(self.user_path):
-            self.commit_dict = {}#dict for each repo to store commit data
-            dir_path = directory[0]
-            commits_files = directory[-1]
-            for commits_file in commits_files:
-                self.call_commits(dir_path, commits_file)
-            json_obj = json.dumps(self.commit_dict)
-            with open(os.path.join(dir_path, "commit_data.json")) as f:
-                json.dump(json_obj, f, indent=4)
+        #get all directories listed in data/user_name and skip the first one (=data/user_name)
+        dirs = [(direc[0], direc[-1]) for direc in os.walk(self.user_path)][1:]
+        for dir_path, commits_files in dirs:
+            commit_dict = self.call_commits(dir_path, commits_files)
+            proj_name = dir_path.split("/")[-1]
+            with open(os.path.join(self.commits_path, "%s_commit_data.json"%proj_name), "w") as outf:
+                json.dump(commit_dict, outf, indent=4)
+            print("done with path: %s" %dir_path)
 
-    def call_commits_actual(self, path_to_file, com_file):
-        #with open(os.path.join(path_to_file, com_file)) as infile:
-        #test only with one project
-        test_path = "data/defunkt/ace/master_commits.json"
-        with open(test_path) as infile:
-            commits = json.load(infile)
-            for commit in commits:
-                #print(commit["url"])
-                self._get_commit_data(commit["url"])
-    -> hier weiter machen!! nicht alles speichen sondern direct ansprechen und
-    dann mit _get_commit_data weiter
-
-    def call_commits(self,path_to_file,com_file):
-        self._get_commit_data("testing")
+    def call_commits(self, path_to_files, commits_files):
+        ret = dict()
+        for commits_file in commits_files:
+            with open(os.path.join(path_to_files, commits_file)) as infile:
+                print(path_to_files, commits_file)
+                commits = json.load(infile)
+                for commit in commits:
+                    sha2data_dict = self._get_commit_data(commit["url"])
+                    for sha, data in sha2data_dict.items():
+                        ret[sha] = data
+        return ret
 
     def _get_commit_data(self, commit_url):
+        ret = dict()
         cmd = "curl -l -H 'Authorization: token %s' \
                 %s" %(self.auth_token, commit_url)
-        #com_data = retrieve_json(cmd) takes wayyy to long!!
-        #test with existing file:
-        #print(com_data)
-        with open("tmp.json", "r") as infile:
-            for single_commit in json.load(infile):#change to for single_commit in com_data
-                tmp_dict = {}
-                tmp_dict["commit_author_date"] = single_commit["commit"]["author"]["date"]
-                tmp_dict["commit_committer_date"] = single_commit["commit"]["committer"]["date"]
-                tmp_dict["author_login"] = single_commit["author"]["login"]
-                tmp_dict["committer_login"] = single_commit["committer"]["login"]
-                tmp_dict["stats"] = single_commit["stats"]
-                tmp_dict["file_count"] = len(single_commit["files"])
-                self.commit_dict[single_commit["sha"]] = tmp_dict
-
-
-
-
-
+        data_dict = retrieve_json(cmd)
+        tmp_dict = dict()
+        tmp_dict["author_name"] = data_dict["commit"]["author"]["name"]
+        tmp_dict["commit_author_date"] = data_dict["commit"]["author"]["date"]
+        tmp_dict["committer_name"] = data_dict["commit"]["committer"]["name"]
+        tmp_dict["commit_committer_date"] = data_dict["commit"]["committer"]["date"]
+        try:
+            tmp_dict["author_login"] = data_dict["author"]["login"]
+            tmp_dict["committer_login"] = data_dict["committer"]["login"]
+        except:
+            #self.logger.error("author_login and/or committer_login linking to Null: %s"%commit_url)
+            print("could not retrieve author_login and/or committer_login for: %s"%commit_url)
+            tmp_dict["author_login"] = "null"
+            tmp_dict["committer_login"] = "null"
+        tmp_dict["stats"] = data_dict["stats"]
+        tmp_dict["file_count"] = len(data_dict["files"])
+        ret[data_dict["sha"]] = tmp_dict
+        return ret
 
 if __name__=="__main__":
     rcd = RepoCommitData("defunkt")
     rcd.walk_dirs()
-
-"""
-
-for each project
-    for each commit in _commitsfile:
-        call commit["url"]:
-
-append to global file for project:
-
-for commit:
-    [sha] -> existing?
-    [commit][author][date]
-    [commit][committer][date]
-    [author][login] = author
-    [committer][login] = committer
-    bool isEqual(author, committer)
-
-    [stats][total]
-    [stats][additions]
-    [stats][deletions]
-    len of [files] (how many)
-
-"""
